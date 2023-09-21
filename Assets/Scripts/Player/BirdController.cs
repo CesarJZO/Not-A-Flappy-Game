6247fb5814958e409c1ee62be83fe83932ed572a
César Zárate <cesarjzo@outlook.com>
2023-09-20 18:57:28 -0600

    Add bird script code and add it to the scene object

--- Assets/Scripts/Player/BirdController.cs
+++ Assets/Scripts/Player/BirdController.cs
@@ -1,9 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Player
 {
     public class BirdController : MonoBehaviour
     {
-                
+        [SerializeField] private float jumpStrength;
+
+        private Rigidbody2D _rigidbody2D;
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _rigidbody2D.velocity = Vector2.up * jumpStrength;
+            }
+        }
     }
 }
